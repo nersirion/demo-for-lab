a179ec73deb7c5d36d79b4a435b856a6e295222d
nersirion <nersirion@gmail.com>
2020-05-10 22:37:24 +0300

    start writing api wrapper for put charts in excel

--- src/chart.py
+++ src/chart.py
@@ -1,28 +1,56 @@
 
-def set_writer(data_to_excel:dict) -> dict:
-    writer = pd.ExcelWriter(save_path, engine="xlsxwriter")
-    workbook = writer.book
-    for i, (name, df) in enumerate(data_to_excel):
-        df.to_excel(writer, sheet_name=name)
-        worksheets = {name: writer.sheets[name]}
-    return worksheets
+class ExcelWriterWrapper:
+    
+    def __init__(self, save_path:str, data_to_excel:dict):
+        self.writer = pd.ExcelWriter(save_path, engine="xlsxwriter")
+        self.workbook = writer.book
+        self.worksheets = create_worksheets(data_to_excel)
+
+    def __call__(self, name):
+        return self.worksheets["name"]
+
+    @classmethod
+    def create_worksheets(cls, data_to_excel:dict) -> dict:
+        for i, (name, df) in enumerate(data_to_excel):
+            df.to_excel(writer, sheet_name=name)
+            worksheets = {name: writer.sheets[name]}
+        return worksheets
+
+    def close_writer(self):
+        self.writer.close()
+
+def set_standart_chart_options(workbook, name:str):
+    workbook.add_chart({'type': 'scatter',
+                              'subtype': 'smooth'})
+    chart.set_size({'x_scale': 1.6, 'y_scale': 1.4})
+    chart.set_title({'name': name})
+    name_font = {'name_font':{'name': 'Times New Roman', 'size': 12}}
+    chart.set_x_axis(name_font)
+    chart.set_y_axis(name_font)
+
+def add_series_with_default_options():
+    generator = Endless()
+    chart.add_series({'line': {"width": 4},
+                      "smooth": True,
+                      "marker": next(generator.marker()),
+                      "size": 6,
+                      "border": {"color": "black"},
+                      "fill": {'color': next(generator.color()))}})
+
+def add_data_on_chart():
+    data = chart.series[ind_series] 
+    data['name'] = f'{data_for_charts['line']}/{name}'
+    data['categoreies'] = None
+    data['values'] = None
+    
 
 def set_chart_options():
-    chart=workbook.add_chart({'type': 'scatter',
-                              'subtype': 'smooth'})
-    chart.set_size({'x_scale': 2, 'y_scale': 2})
     chart.set_title({'name': 'Разные циклы'})
     chart.set_x_axis({'name': 'Voltage',
-                      'name_font': {
-                                'name': 'Times New Roman',
-                                'size': 12
-                                },
                       'min': 2.4,
                       'max': 4,
                       'reverse': True})
     chart.set_y_axis({'name': names[i],
-                      'name_font':
-                     {'name': 'Times New Roman', 'size': 12}})
     worksheet.insert_chart('N'+str(30+i*30), chart) 
 
 def add_data_on_chart():
@@ -50,3 +78,35 @@ class Endless:
 
     def next_marker(self):
         return next(self.marker)
+class ChartMaker(ExcelWriterWrapper):
+    
+    def __init__(self, charts_list:list, chart_parametrs:dict):
+        super().__init__()
+        self.charts = charts_list
+
+    def __len__(self):
+        return len(self.charts)
+
+    def __getitem__(self, ind_chart):
+        name = self.charts[ind_chart][0]
+        self.add_chart(name)
+        x_axis_name = self.charts[ind_chart][1]
+        y_axis_name =self.charts[ind_chart][2]
+        return (x_axis_name, y_axis_name)
+
+    def add_chart(self, name):
+        set_standart_chart_options(self.workbook ,name)
+
+class GittCharts(ChartMaker):
+    
+    def get_categories(self, )
+
+    def insert_data(self):
+        for i, (x_axis_name, y_axis_name) in enumerate(self):
+            chart = self.workbook.charts[i]
+            update_chart(chart)
+            for j in range():
+                add_series_with_default_options(chart)
+                update_series
+
+
